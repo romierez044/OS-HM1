f19df5a32c2df9a28307fd7c9998cbf9837ec25a
Romi Erez <romi.erez@cidersecurity.io>
2022-03-07 18:48:57 +0200

    dummy

--- main.c
+++ main.c
@@ -7,11 +7,16 @@ int readFileAndUpdateMergedFile(char *fileName, FILE *mergedFile);
 void report_input_file(const char* file_name, int num_stud);
 void report_data_summary(int num_stud, double avg);
 void updateGradesInMergedFile(char *student_line, FILE *mergedFile);
+void insert_grades_in_position(char *student_line, long position, FILE *mergedFile);
 
 char mergedFileName[] = "merged.txt";
+long size;
+long position;
 
 int main(int args, char **argv) {
-    FILE *mergedFile = fopen(mergedFileName, "w");
+    FILE *mergedFile = fopen(mergedFileName, "r+");
+    setlinebuf(mergedFile);
+    size = ftell(mergedFile); printf("after open. %d\n",size);
     for (int i = 1; i < args; ++i) {
         readFileAndUpdateMergedFile(argv[i], mergedFile);
     }
@@ -44,7 +49,7 @@ int readFileAndUpdateMergedFile(char *fileName, FILE *mergedFile) {
         updateGradesInMergedFile(line_buf, mergedFile);
         line_size = getline(&line_buf, &line_buf_size, fp);
     }
-    report_input_file(fileName, line_count);
+//    report_input_file(fileName, line_count);
     free(line_buf);
     line_buf = NULL;
     fclose(fp);
@@ -52,28 +57,32 @@ int readFileAndUpdateMergedFile(char *fileName, FILE *mergedFile) {
 }
 
 void updateGradesInMergedFile(char *student_line, FILE *mergedFile) {
+    size=ftell(mergedFile);
+    printf ("updateGradesInMergedFile Size of myfile.txt: %ld bytes.\n",size);
     int student_found = 0;
     char seps[] = " ";
-    char *token_merged_file;
-    char *token_input_file;
+    char *merged_file_token;
+    char *input_file_token;
     /* getting student name from input file */
     char line_copy[80];
     strcpy(line_copy, student_line);
-    token_input_file = strtok(line_copy, seps);
+    input_file_token = strtok(line_copy, seps);
 
     char *line_buf = NULL;
     size_t line_buf_size = 0;
     ssize_t line_size;
-
+    size = ftell(mergedFile); printf("before getline. %d\n",size);
     line_size = getline(&line_buf, &line_buf_size, mergedFile);
+    size = ftell(mergedFile); printf("after getline.%d\n", size);
+
     while (line_size >= 0 && student_found == 0)
     {
         /* getting student name from merged file */
-        token_merged_file = strtok(line_buf, seps);
+        merged_file_token = strtok(line_buf, seps);
         /* compare students name */
-        if (strcmp(token_merged_file, token_input_file)==0) {
-            // TODO - adding the grapdes in the end of the line
-            /* student already in the merged file */
+        if (strcmp(merged_file_token, input_file_token) == 0) { /* student already in the merged file */
+            position = ftell(mergedFile);
+            insert_grades_in_position(student_line, position, mergedFile);
             student_found = 1;
         }
         line_size = getline(&line_buf, &line_buf_size, mergedFile);
@@ -82,13 +91,37 @@ void updateGradesInMergedFile(char *student_line, FILE *mergedFile) {
     line_buf = NULL;
 
     /* student isn't exist in the merged file */
-    fseek(mergedFile, 0, SEEK_END);
-    fprintf(mergedFile, "%s", student_line);
+    if (student_found == 0){
+        fseek(mergedFile, 0, SEEK_END);
+        fprintf(mergedFile, "%s", student_line);
+    }
+
     /* back to the start of the merged file */
     fseek(mergedFile, 0, SEEK_SET);
 }
 
+void insert_grades_in_position(char *student_line, long position, FILE *mergedFile) {
+    fseek(mergedFile, 0L, SEEK_END);
+    long Fin = ftell(mergedFile);
+    fseek(mergedFile, position, SEEK_SET);
+    long lSize = Fin - position;
+    char *buffer = (char*) malloc(sizeof(char) * lSize);
+    fread(buffer, 1, lSize, mergedFile);
+    fseek(mergedFile, position - 1, SEEK_SET);
 
+    char *input_file_token;
+    char seps[] = " ";
+    /* get first grade */
+    strtok(student_line, seps);
+    input_file_token = strtok(NULL, seps );
+    while(input_file_token != NULL ) {
+        fprintf(mergedFile, " %s\n", input_file_token);
+        fflush(mergedFile);
+        input_file_token = strtok(NULL, seps );
+    }
+    fputs(buffer, mergedFile);
+    fflush(mergedFile);
+}
 
 void report_input_file(const char* file_name, int num_stud)
 {
@@ -102,12 +135,3 @@ void report_data_summary(int num_stud, double avg)
                     " %d grade average: %.2f\n",
             getpid(), num_stud, avg);
 }
-
-
-//    char seps[] = " ";
-//    char *token;
-//    token = strtok( student_line, seps );
-//    while( token != NULL ) {
-//
-//        token = strtok(NULL, seps ); //get next token
-//    }
